55b646d1824a7af433eb53d8658c57fdfeb2d30b
MattBurnsUk <hello@matt-burns.com>
2019-08-06 15:20:37 -0700

    created a temporary data file with an array of objects

---
+++ app/listData.js
@@ -0,0 +1,22 @@
+const listData = [
+    {
+        id: 1,
+        store: "SaveOnFoods",
+        itemText: "bread"
+    },
+    {
+        id: 2,
+        store: "SaveOnFoods",
+        itemText: "milk"
+    },
+    {
+        id: 3,
+        store: "HomeDepo",
+        itemText: "nails"
+    },
+    {
+        id: 4,
+        store: "HomeDepo",
+        itemText: "wood"
+    }
+]
