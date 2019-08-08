ee10427c441938326a6144c45bcd255381b3d736
MattBurnsUk <hello@matt-burns.com>
2019-08-08 06:57:44 -0700

    further refactoring. See previous commit

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-function ListItem(props){
-    return (
-        <div>
-            <input type="checkbox" />
-            <li>{props.itemText}</li>
-        </div>
-    )
-}
+class ListItem extends React.Component {
+    render() {
+        return (
+            <div>
+                <input type="checkbox" />
+                <li>{this.props.itemText}</li>
+            </div>
+        )
+        }
+    }
 
 export default ListItem;
